b2eb356656f7d3ad0c5920198ea59e050ecdc6fa
Dev Bhupendra Pandya <devpandya@mac.home>
2026-01-09 12:24:46 -0500

    [starterCode] feat: Added boiler plate code for Springboot with some dependencies

---
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/SmartPayBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.SmartPay_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPayBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPayBackEndApplication.class, args);
+	}
+
+}
