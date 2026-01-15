75892a5c846ba5d40fc646df1c4f709b69a13797
leo juarez <sk8leo@live.com>
2026-01-15 16:37:46 -0500

    created RegisterUserDTO

---
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/domain/dtos/RegisterUserDTO.java
@@ -0,0 +1,14 @@
+package com.fdmgroup.SmartPay_BackEnd.domain.dtos;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDTO {
+
+    private String email;
+    private String password;
+
+}
