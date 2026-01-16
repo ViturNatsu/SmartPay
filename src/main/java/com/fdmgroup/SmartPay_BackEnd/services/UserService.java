a04bf027ed74f7e901239182a28600ab97613d0d
LasimaP <lasimap@gmail.com>
2026-01-16 09:47:48 -0500

    add user service

--- src/main/java/com/fdmgroup/SmartPay_BackEnd/services/UserService.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/services/UserService.java
@@ -1,4 +1,7 @@
 package com.fdmgroup.SmartPay_BackEnd.services;
 
+import com.fdmgroup.SmartPay_BackEnd.domain.entities.User;
+
 public interface UserService {
+    String signUpUser(User user);
 }
--- src/main/java/com/fdmgroup/SmartPay_BackEnd/services/impl/UserServiceImpl.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/services/impl/UserServiceImpl.java
@@ -1,7 +1,26 @@
 package com.fdmgroup.SmartPay_BackEnd.services.impl;
 
+import com.fdmgroup.SmartPay_BackEnd.domain.entities.User;
+import com.fdmgroup.SmartPay_BackEnd.repositories.UserRepository;
 import com.fdmgroup.SmartPay_BackEnd.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl implements UserService {
     // TODO
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public String signUpUser(User user) {
+
+        userRepository.save(user);
+        return "returned token";
+    }
 }
