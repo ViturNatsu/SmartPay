c3567cfa14db68dd3fb5289e1b2d21cc2e46b5cc
Karna Errande <Karna.Errande@fdmgroup.com>
2026-01-16 08:03:13 -0800

    removed permissions from protected route

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,11 +1,8 @@
 import { useAuth } from "../context/AuthContext";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
-interface ProtectedRouteProps {
-  requiredPermissions?: string[];
-}
 
-const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ requiredPermissions }) => {
+const ProtectedRoute: React.FC = () => {
   const location = useLocation();
   const { user } = useAuth();
 
