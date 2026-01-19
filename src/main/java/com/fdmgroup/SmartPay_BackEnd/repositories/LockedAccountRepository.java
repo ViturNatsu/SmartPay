8b6001df96d54b58311a24fe3864caa371dc1c0f
Christopher Brine <chrisbrine1999@gmail.com>
2026-01-19 15:16:45 -0500

    [US-F02-04-02] feat: Added email format validation to PasswordResetService

---
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/repositories/LockedAccountRepository.java
@@ -0,0 +1,9 @@
+package com.fdmgroup.SmartPay_BackEnd.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fdmgroup.SmartPay_BackEnd.domain.entities.LockedAccount;
+
+public interface LockedAccountRepository extends JpaRepository<Long, LockedAccount> {
+	
+}
