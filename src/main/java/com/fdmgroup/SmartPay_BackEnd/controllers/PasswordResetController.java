94263d87aeb9da74edaf77ed50d0074cb51ff5de
Sangwook Lee <sangwook.lee@fdmgroup.com>
2026-01-19 14:48:16 -0500

    [US-F02-04-02] feat: add base controller and service classes for password reset

--- src/main/java/com/fdmgroup/SmartPay_BackEnd/controllers/PasswordResetController.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/controllers/PasswordResetController.java
@@ -1,5 +1,28 @@
 package com.fdmgroup.SmartPay_BackEnd.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fdmgroup.SmartPay_BackEnd.services.PasswordResetService;
+
+@RestController
+@RequestMapping("api/v1/password-reset")
 public class PasswordResetController {
-    //TODO
+    
+	private PasswordResetService service;
+
+	public PasswordResetController(PasswordResetService service) {
+		super();
+		this.service = service;
+	}
+	
+	@PostMapping
+	public ResponseEntity<String> startResetRequest(@RequestBody String email) {
+		service.startResetRequest(email);
+		return ResponseEntity.accepted().build();
+	}
+	
 }
---
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/domain/entities/AccountLock.java
@@ -0,0 +1,71 @@
+package com.fdmgroup.SmartPay_BackEnd.domain.entities;
+
+import java.util.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class AccountLock {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ACCOUNTLOCK_SEQ_GEN")
+	@SequenceGenerator(name = "ACCOUNTLOCK_SEQ_GEN", sequenceName = "ACCOUNTLOCK_SEQ_GEN")
+	private long id;
+	private String email;
+	private Date locked_at;
+	
+	public AccountLock() {
+		super();
+	}
+
+	public AccountLock(long id, String email, Date locked_at) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.locked_at = locked_at;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getLocked_at() {
+		return locked_at;
+	}
+
+	public void setLocked_at(Date locked_at) {
+		this.locked_at = locked_at;
+	}
+
+	@Override
+	public String toString() {
+		return "AccountLock [id=" + id + ", email=" + email + ", locked_at=" + locked_at + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, locked_at);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountLock other = (AccountLock) obj;
+		return Objects.equals(email, other.email) && id == other.id && Objects.equals(locked_at, other.locked_at);
+	}
+
+}
