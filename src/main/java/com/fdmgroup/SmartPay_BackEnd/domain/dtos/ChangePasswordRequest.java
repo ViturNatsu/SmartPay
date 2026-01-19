cbc5b94605e5f60b9b9a6e35216a2bcd16f077ae
Ajay.Sodhi <ajay.sodhi@fdmgroup.com>
2026-01-19 16:51:41 -0500

    Created ChangePasswordDTO and modified ConfirmCodeDTO

--- src/main/java/com/fdmgroup/SmartPay_BackEnd/domain/dtos/ChangePasswordRequest.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/domain/dtos/ChangePasswordRequest.java
@@ -5,17 +5,22 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public class ConfirmCodeDTO {
+public class ChangePasswordRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
 
-    @JsonProperty("access-code")
-    private String accessCode;
+    @NotBlank(message = "Password is required")
+    private String password1;
 
-    public ConfirmCodeDTO() {
-    }
+    @NotBlank(message = "Please confirm your password")
+    private String password2;
+
+    @NotBlank(message = "Access code is required")
+    @JsonProperty("access-code")
+    @NotBlank(message = "Access code is required")
+    private String accessCode;
 
     public String getEmail() {
         return email;
@@ -23,6 +28,22 @@ public class ConfirmCodeDTO {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public void setPassword2(String password2) {
+        this.password2 = password2;
     }
 
     public String getAccessCode() {
