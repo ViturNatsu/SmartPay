512c76d73eed11c0626e65304ef7e21f2af93504
Varni.Patel <Varni.Patel@fdmgroup.com>
2026-01-19 16:00:06 -0500

    Implemented Backend 409 response

--- src/main/java/com/fdmgroup/SmartPay_BackEnd/controllers/RegistrationController.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/controllers/RegistrationController.java
@@ -4,6 +4,8 @@ import com.fdmgroup.SmartPay_BackEnd.domain.dtos.RegisterUserDTO;
 import com.fdmgroup.SmartPay_BackEnd.domain.entities.User;
 import com.fdmgroup.SmartPay_BackEnd.services.RegistrationService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +18,19 @@ public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping
-    public User register(@RequestBody RegisterUserDTO userDto) {
-        return registrationService.register(userDto);
+    public ResponseEntity<?> register(@RequestBody RegisterUserDTO userDto) {
+        try{
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(registrationService.register(userDto));
+        }
+        catch(RuntimeException e){
+            //System.out.println(e.getMessage());
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+
+
+
     }
 }
 
--- src/main/java/com/fdmgroup/SmartPay_BackEnd/services/RegistrationService.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/services/RegistrationService.java
@@ -1,5 +1,6 @@
 package com.fdmgroup.SmartPay_BackEnd.services;
 
+import com.fdmgroup.SmartPay_BackEnd.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import com.fdmgroup.SmartPay_BackEnd.domain.dtos.RegisterUserDTO;
@@ -7,13 +8,22 @@ import com.fdmgroup.SmartPay_BackEnd.domain.entities.User;
 
 import lombok.AllArgsConstructor;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class RegistrationService {
     private final UserService userService;
+    private final UserRepository userRepository;
 
     public User register(RegisterUserDTO userDto) {
-        User user = new User(userDto.getEmail(), userDto.getPassword());
-        return userService.signUpUser(user);
+        List<User> users =  userRepository.findByEmail(userDto.getEmail());
+        if(users.isEmpty()){
+            User user = new User(userDto.getEmail(), userDto.getPassword());
+            return userService.signUpUser(user);
+        }
+        else {
+            throw new RuntimeException("Email already in use");
+        }
     }
 }
