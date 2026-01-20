0d6449ef8d85dd945b47b3d93ea98582a4831fa1
LasimaP <lasimap@gmail.com>
2026-01-20 12:48:22 -0500

    added custom email error message

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,65 +1,68 @@
 import { useState } from "react";
-import axios from 'axios';
-export const Register = () =>{
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(password !== confirmPassword){
-            alert("passwords do not match. ")
-            return
-        }
-        const userInfo = {
-            email: email,
-            password: password
-        }
-        const api = 'http://localhost:8080/api/v1/registration';
-        
-        const auth = {
-            username: 'admin',
-            password: 'admin'
-        }
-        axios.post(api, userInfo,auth)
-            .then(response => {
-                console.log('success')
-            })
-            .catch(error => {
-                console.log('error')
-            })
+import axios from "axios";
+export const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("passwords do not match. ");
+      return;
     }
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                type="email"
-                placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                />
+    const userInfo = {
+      email: email,
+      password: password,
+    };
+    const api = "http://localhost:8080/api/v1/registration";
 
-                <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                />
+    const auth = {
+      username: "admin",
+      password: "admin",
+    };
+    axios
+      .post(api, userInfo, auth)
+      .then((response) => {
+        console.log("success");
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
 
-                <input
-                type="password"
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                />
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onInvalid={(e) =>
+            e.target.setCustomValidity(
+              "Enter a valid email address (example: name@domain.com).",
+            )
+          }
+        />
 
-                <button type="submit">
-                Create Account
-                </button>
-            </form>
-        </div>
-    )
-}
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  );
+};
