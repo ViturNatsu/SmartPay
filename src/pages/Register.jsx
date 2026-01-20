8f3829012786271d8a43a77ab5172163d477a9a2
Leonardo Juarez <leonardo.juarez@fdmgroup.com>
2026-01-20 20:52:24 +0000

    Merge branch 'feature/02-01-01-register-account' into 'develop'

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,68 @@
+import { useState } from "react";
+import axios from "axios";
+export const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-export const Register = () =>{
-    return <h1>Register</h1>
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("passwords do not match. ");
+      return;
+    }
+
+    const userInfo = {
+      email: email,
+      password: password,
+    };
+    const api = "http://localhost:8080/api/v1/registration";
+
+    const auth = {
+      username: "admin",
+      password: "admin",
+    };
+    axios
+      .post(api, userInfo, auth)
+      .then((response) => {
+        console.log("success");
+      })
+      .catch((error) => {
+        console.log("error");
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          onInvalid={(e) =>
+            e.target.setCustomValidity(
+              "Enter a valid email address (example: name@domain.com).",
+            )
+          }
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <button type="submit">Create Account</button>
+      </form>
+    </div>
+  );
+};
