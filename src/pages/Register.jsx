69ec957571af561772ff4c6ac1f64f9f81dfde21
leo juarez <sk8leo@live.com>
2026-01-21 14:16:07 -0500

    [02-01-01] style: layout using box, using material ui for components

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Box, Button, Grid, TextField } from "@mui/material";
 import axios from "axios";
 export const Register = () => {
   const [email, setEmail] = useState("");
@@ -33,36 +34,50 @@ export const Register = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          onInvalid={(e) =>
-            e.target.setCustomValidity(
-              "Enter a valid email address (example: name@domain.com).",
-            )
-          }
-        />
+    <Grid container spacing={2}>
+      <Grid size={6}>
+        <h1>Register</h1>
+      </Grid>
+      <Grid size={6}>
+        <Box display="flex" flexDirection="column" alignItems={"center"}  gap={2}>
+          <h1>Create Your Account</h1>
+          <p>Sign up to start managing your finances with SmartPay</p>
+          <form onSubmit={handleSubmit}>
+            <Box display="flex" flexDirection="column" gap={2}>
+              <TextField
+                label="Email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onInvalid={(e) =>
+                  e.target.setCustomValidity(
+                    "Enter a valid email address (example: name@domain.com).",
+                  )
+                }
+              />
 
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+              <TextField
+                type="password"
+                label="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
 
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-        />
+              <TextField
+                type="password"
+                label="Confirm Password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
 
-        <button type="submit">Create Account</button>
-      </form>
-    </div>
+              <Button  variant="contained"  type="submit">
+                Create Account
+              </Button>
+            </Box>
+          </form>
+          <p>Already have an account? Sign in</p>
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
