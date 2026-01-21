88ab1b57faf9e52429434e955af2a8a98a04aec1
Piotr Drozd <piotr.drozd@fdmgroup.com>
2026-01-21 16:31:30 +0000

    Update SecurityConfig.java

--- src/main/java/com/fdmgroup/SmartPay_BackEnd/config/SecurityConfig.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/config/SecurityConfig.java
@@ -36,7 +36,6 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.PUT,"/api/v*/password-reset/**").permitAll()
                 .requestMatchers("/api/v*/password-reset","/api/v*/password-reset/**","/api/v*/registration/**","/api/v*/email/sendemail/**","/h2-console/**", "/error/**").permitAll()
                 .anyRequest().authenticated()
-        src/main/java/com/fdmgroup/SmartPay_BackEnd/config/SecurityConfig.java
                 )
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
                 .httpBasic(Customizer.withDefaults());
