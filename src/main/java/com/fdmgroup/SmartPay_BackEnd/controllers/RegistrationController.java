33651ac9c1a7aa385edf99777d63808085205926
leo juarez <sk8leo@live.com>
2026-01-26 10:41:47 -0500

    [02-01-01] fix: readding swagger documentation, fixed endpoint for registration controller

--- src/main/java/com/fdmgroup/SmartPay_BackEnd/controllers/RegistrationController.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/controllers/RegistrationController.java
@@ -4,6 +4,9 @@ import com.fdmgroup.SmartPay_BackEnd.domain.dtos.RegisterUserDTO;
 import com.fdmgroup.SmartPay_BackEnd.domain.entities.User;
 import com.fdmgroup.SmartPay_BackEnd.exception.DuplicateEmailException;
 import com.fdmgroup.SmartPay_BackEnd.services.RegistrationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,12 +20,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/auth/register")
+@RequestMapping("api/v1/registration")
 @AllArgsConstructor
 public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping
+    @Operation(summary = "Register and create new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User registered/created."),
+            @ApiResponse(responseCode = "429", description = "Too many requests. Rate limit exceeded."),
+            @ApiResponse(responseCode = "409", description = "User email already exists, cannot create an account with a duplicate email")
+    })
+    @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "User creation payload", required = true)
     public ResponseEntity<User> register(@Valid @RequestBody RegisterUserDTO userDto) {
         if (!userDto.getPassword().equals(userDto.getConfirmPassword())) {
             throw new IllegalArgumentException("Password and confirm password do not match");
