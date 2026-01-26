1cf46dade066f023a446a2f49c627c49d90e1e22
leo juarez <sk8leo@live.com>
2026-01-26 11:27:40 -0500

    [02-01-01-register] fix: using correct path

--- src/main/java/com/fdmgroup/SmartPay_BackEnd/controllers/RegistrationController.java
+++ src/main/java/com/fdmgroup/SmartPay_BackEnd/controllers/RegistrationController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/registration")
+@RequestMapping("api/v1/auth/register")
 @AllArgsConstructor
 public class RegistrationController {
     private final RegistrationService registrationService;
