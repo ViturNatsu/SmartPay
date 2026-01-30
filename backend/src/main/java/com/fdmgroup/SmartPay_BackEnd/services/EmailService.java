596eb95bd85946e2a32c17e0e38277c319f7a635
leo juarez <sk8leo@live.com>
2026-01-30 11:23:45 -0500

    hotfix: enable email to be async

--- backend/src/main/java/com/fdmgroup/SmartPay_BackEnd/services/EmailService.java
+++ backend/src/main/java/com/fdmgroup/SmartPay_BackEnd/services/EmailService.java
@@ -3,6 +3,6 @@ package com.fdmgroup.SmartPay_BackEnd.services;
 import com.fdmgroup.SmartPay_BackEnd.domain.entities.EmailDetails;
 
 public interface EmailService {
-    String sendSimpleMail(EmailDetails details);
+    void sendSimpleMail(EmailDetails details);
 
 }
