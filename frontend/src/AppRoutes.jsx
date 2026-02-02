2531e02eda66e5c59b5e5d738f30d1fbb54bc9be
Tommy Huynh <tommy.huynh@fdmgroup.com>
2026-02-02 18:07:41 +0000

    US-F03-04-03 Return to home dashboard from any screen

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -7,6 +7,11 @@ import { VerifyEmail } from "./pages/VerifyEmail";
 import { VerifyOtp } from "./pages/VerifyOtp";
 import ProtectedRoute from "./routes/ProtectedRoute";
 import { Home } from "./pages/Home";
+import { Accounts } from './pages/Accounts';
+import { Transactions } from './pages/Transactions';
+import { Cards } from './pages/Cards';
+import { Reports } from './pages/Reports';
+import { Settings } from './pages/Settings';
 import { CreateAccount } from "./pages/CreateAccount";
 import { AddPayee } from "./pages/AddPayee";
 import { MakeAPayment } from "./pages/MakeAPayment";
@@ -16,19 +21,24 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/verify-email" element={<VerifyEmail />} />
-          <Route path="/verify" element={<VerifyOtp />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/create-account" element={<CreateAccount />} />
-            <Route path="/view-history" element={<ViewHistory />} />
-            <Route path="/make-a-payment" element={<MakeAPayment />} />
-            <Route path="/add-payee" element={<AddPayee />} />
-          </Route>
+      <Route path="/register" element={<Register />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/verify-email" element={<VerifyEmail />} />
+      <Route path="/verify" element={<VerifyOtp />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/accounts" element={<Accounts />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/view-history" element={<ViewHistory />} />
+        <Route path="/make-a-payment" element={<MakeAPayment />} />
+        <Route path="/add-payee" element={<AddPayee />} />
+      </Route>
     </Routes>
   );
 }
---
+++ frontend/src/pages/Transactions.jsx
@@ -0,0 +1,7 @@
+import Navbar from "../components/Navbar"
+export const Transactions = () => {
+    return <>
+        <Navbar />
+        <h1>Transactions Page</h1>
+    </>
+}
