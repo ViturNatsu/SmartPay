b40d84c9484d49f6ce1dad65390a2ebab29f8b15
Christopher Raleigh <craleigh318@comcast.net>
2026-02-04 09:18:33 -0800

    Start 1st test for RegistrationTest

--- backend/src/test/java/com/fdmgroup/SmartPay_BackEnd/RegistrationTest.java
+++ backend/src/test/java/com/fdmgroup/SmartPay_BackEnd/RegistrationTest.java
@@ -1,24 +1,63 @@
 package com.fdmgroup.SmartPay_BackEnd;
 
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fdmgroup.SmartPay_BackEnd.domain.dtos.SignUpDTO;
 import com.fdmgroup.SmartPay_BackEnd.repositories.UserRepository;
+import com.fdmgroup.SmartPay_BackEnd.services.RegistrationService;
 import com.fdmgroup.SmartPay_BackEnd.services.UserService;
-import com.fdmgroup.SmartPay_BackEnd.services.impl.RegistrationServiceImpl;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@ExtendWith(MockitoExtension.class)
-public class RegistrationTest {
-    @Mock
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(classes = SmartPayBackEndApplication.class)
+@AutoConfigureMockMvc
+@Transactional
+class RegistrationTest {
+    private final String firstName = "Quality";
+    private final String lastName = "Assurance";
+    private final String institution = "Chase";
+    private final String email = "qa@smartpay.test";
+    private final String password = "@wTJGT&a1qn@e38X";
+    @Autowired
+    MockMvc mvc;
+    @Autowired
     private UserService userService;
-
-    @Mock
+    @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RegistrationService registrationService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-    @InjectMocks
-    private RegistrationServiceImpl registrationService;
+    @Test
+    void register() throws Exception {
+        var requestBody = new SignUpDTO();
+        requestBody.setFirstName(firstName);
+        requestBody.setLastName(lastName);
+        requestBody.setInstitution(institution);
+        requestBody.setEmail(email);
+        requestBody.setPassword(password);
+        requestBody.setConfirmPassword(password);
+        var request = buildRegisterRequest(requestBody);
+
+        mvc.perform(request).andExpect(status().isOk());
+    }
+
+    private MockHttpServletRequestBuilder buildRegisterRequest(SignUpDTO requestBody) throws JsonProcessingException {
+        var requestString = objectMapper.writeValueAsString(requestBody);
+        return MockMvcRequestBuilders.post("/api/v1/auth/register").contentType(MediaType.APPLICATION_JSON)
+                .content(requestString);
+    }
+
     /*
     @Test
     void register_returnsUser_whenEmailDoesNotExist() {
