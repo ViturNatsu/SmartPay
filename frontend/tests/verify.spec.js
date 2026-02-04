d683925325dc4d68ffd3d56b87914ee7db0fce8a
LasimaP <lasimap@gmail.com>
2026-02-04 10:00:59 -0500

    refactored tests and added test for verification code reuse

--- frontend/tests/verify.spec.js
+++ frontend/tests/verify.spec.js
@@ -17,6 +17,38 @@ async function fillRegisterForm(page, email, password, confirm) {
   await page.getByRole("button", { name: "Create Account" }).click();
 }
 
+async function getVerificationCode(request, targetEmail) {
+  let emailBody = "";
+
+  await expect.poll(async () => {
+    const res = await request.get(
+      "http://localhost:8025/api/v2/messages"
+    );
+    const json = await res.json();
+
+    if (json.items.length === 0) return null;
+
+    const email = json.items.find(m =>
+      m.Content.Headers.To?.some((to) =>
+        to.includes(targetEmail)
+      )
+    );
+    if (!email) return null;
+
+    emailBody = email.Content.Body;
+    return emailBody;
+  }, {
+    timeout: 10_000,
+    intervals: [500],
+  }).not.toBeNull();
+
+  const match = emailBody.match(/(\d{7})/);
+  expect(match).not.toBeNull();
+
+  const verificationCode = match[0];
+  return verificationCode;
+}
+
 test("user can register successfully and receives email", async ({ page, request }) => {
   const testEmail = "name@domain.com";
 
@@ -53,31 +85,32 @@ test("user can verify email with code from MailHog", async ({ page, request }) =
 
   await expect(page).toHaveURL(/\/verify/);
 
-  let emailBody = "";
-
-  await expect.poll(async () => {
-    const res = await request.get(
-      "http://localhost:8025/api/v2/messages"
-    );
-    const json = await res.json();
-
-    if (json.items.length === 0) return null;
-
-    emailBody = json.items[0].Content.Body;
-    return emailBody;
-  }, {
-    timeout: 10_000,
-    intervals: [500],
-  }).not.toBeNull();
-
-  const match = emailBody.match(/(\d{7})/);
-  expect(match).not.toBeNull();
-
-  const verificationCode = match[0];
+  const verificationCode = await getVerificationCode(request, testEmail);
   console.log("Verification code:", verificationCode);
 
   await page.getByRole("textbox").fill(verificationCode);
   await page.getByRole("button", { name: "Verify Code" }).click();
 
   await expect(page).toHaveURL(/\/login/);
+});
+
+test("user can't re-enter used code", async ({page, request}) => {
+  const testEmail = "name2@domain.com";
+  await fillRegisterForm(page, testEmail, "Password8!", "Password8!");
+
+  await expect(page).toHaveURL(/\/verify/);
+
+  const verificationCode = await getVerificationCode(request, testEmail);
+  console.log("Verification code:", verificationCode);
+
+  await page.getByRole("textbox").fill(verificationCode);
+  await page.getByRole("button", { name: "Verify Code" }).click();
+  await expect(page).toHaveURL(/\/login/);
+
+  await page.goto("http://localhost:5173/verify?email=name2%40domain.com&type=register");
+  await page.getByRole("textbox").fill(verificationCode);
+  await page.getByRole("button", { name: "Verify Code" }).click();
+  
+  await expect(page.getByText("Code has expired or was already used.")).toBeVisible();
+  await expect(page).toHaveURL(/\/verify/);
 });
