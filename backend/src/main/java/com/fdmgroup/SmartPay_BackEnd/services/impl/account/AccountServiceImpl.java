333986e7cdbcf907950aecc67b5e49d7c984ce14
sanskritig08 <sg6479@nyu.edu>
2026-02-10 16:40:17 -0500

    Fixed merge conflicts

--- backend/src/main/java/com/fdmgroup/SmartPay_BackEnd/services/impl/account/AccountServiceImpl.java
+++ backend/src/main/java/com/fdmgroup/SmartPay_BackEnd/services/impl/account/AccountServiceImpl.java
@@ -6,18 +6,16 @@ import java.util.Optional;
 import javax.security.auth.login.AccountNotFoundException;
 
 import org.springframework.beans.BeanUtils;
-import org.springframework.stereotype.Service;
 
 import com.fdmgroup.SmartPay_BackEnd.domain.dtos.account.AccountDto;
 import com.fdmgroup.SmartPay_BackEnd.domain.entities.User;
 import com.fdmgroup.SmartPay_BackEnd.domain.entities.account.Account;
 import com.fdmgroup.SmartPay_BackEnd.domain.entities.account.CheckingAccount;
+import com.fdmgroup.SmartPay_BackEnd.domain.entities.account.SavingsAccount;
 import com.fdmgroup.SmartPay_BackEnd.exception.UserNotFoundException;
 import com.fdmgroup.SmartPay_BackEnd.repositories.UserRepository;
 import com.fdmgroup.SmartPay_BackEnd.repositories.account.AccountRepository;
 import com.fdmgroup.SmartPay_BackEnd.services.account.AccountService;
-
-import jakarta.transaction.Transactional;
 
 public class AccountServiceImpl implements AccountService{
     private AccountRepository accountRepository;
