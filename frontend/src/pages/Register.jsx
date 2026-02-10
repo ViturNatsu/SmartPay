3c7cef51e1a145125485b16e815b4be85ad0d3fc
Christopher Raleigh <craleigh318@comcast.net>
2026-02-10 09:59:50 -0800

    Remove unused setSuccessMessage

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -100,8 +100,6 @@ export const Register = () => {
     setEmailError(isEmailInvalid);
     setPasswordError(isPasswordInvalid);
     setConfirmPasswordError(isConfirmInvalid);
-    // setSuccessMessage("");
-
     if (isEmailInvalid || isPasswordInvalid || isConfirmInvalid) {
       return;
     }
