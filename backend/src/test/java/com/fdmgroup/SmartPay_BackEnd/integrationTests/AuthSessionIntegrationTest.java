b8b2d328a70d235dabaf7ad6aae6c54cd450b09b
Christopher Brine <chrisbrine1999@gmail.com>
2026-03-03 12:43:02 -0500

    [US25-02-01] docs: Updated .gitignore and began integration test implementation

---
+++ backend/src/test/java/com/fdmgroup/SmartPay_BackEnd/integrationTests/AuthSessionIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.fdmgroup.SmartPay_BackEnd.integrationTests;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+
+@SpringBootTest(classes = com.fdmgroup.SmartPay_BackEnd.SmartPayBackEndApplication.class)
+@AutoConfigureMockMvc
+public class AuthSessionIntegrationTest {
+
+}
