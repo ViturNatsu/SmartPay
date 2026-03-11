42053fb3a2f647387e4f66ca56ae37ee9540e516
keshavanand <keshavanandsingh89@gmail.com>
2026-03-11 09:36:09 -0400

    feature/git-pipeline/added-pipeline-and-runner-files

---
+++ gitlab_runner_setup/trigger/trigger.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import subprocess
+import os   # <-- missing import
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def trigger():
+    if request.method == "POST":
+        subprocess.run([
+            "gcloud","run","jobs","execute","gitlab-ci-runner","--region","us-central1"
+        ])
+        return "Job started", 200
+
+    return "Service alive", 200
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
