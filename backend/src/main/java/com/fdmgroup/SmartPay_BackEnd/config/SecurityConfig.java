6bdd8b13463a61d7516432f870f9072cd92e815c
Zeyu Shao <zeyu.shao@hotmail.com>
2026-03-11 15:27:32 -0400

    Added admin only access to admin endpoints

--- backend/src/main/java/com/fdmgroup/SmartPay_BackEnd/config/SecurityConfig.java
+++ backend/src/main/java/com/fdmgroup/SmartPay_BackEnd/config/SecurityConfig.java
@@ -73,10 +73,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-            .csrf(csrf -> csrf.disable())
-                .exceptionHandling(exception ->
-                        exception.authenticationEntryPoint((request, response, authException) -> {
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(csrf -> csrf.disable())
+                .exceptionHandling(
+                        exception -> exception.authenticationEntryPoint((request, response, authException) -> {
                             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
                             response.setContentType("application/json");
                             Map<String, String> errorBody = new HashMap<>();
@@ -85,18 +85,17 @@ public class SecurityConfig {
                             errorBody.put("message", "Missing Or Invalid Token.");
                             ObjectMapper mapper = new ObjectMapper();
                             response.getWriter().write(mapper.writeValueAsString(errorBody));
-                        })
-                )
-            .authorizeHttpRequests(auth -> auth
-                            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                            .requestMatchers(HttpMethod.PUT,"/api/v*/password-reset/**").permitAll()
-                            .requestMatchers(PUBLIC_API_ENDPOINTS).permitAll()
-                            .requestMatchers(DEV_ONLY).permitAll()
-                    .anyRequest().authenticated()
-            )
-            .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                        }))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT, "/api/v*/password-reset/**").permitAll()
+                        .requestMatchers(PUBLIC_API_ENDPOINTS).permitAll()
+                        .requestMatchers(DEV_ONLY).permitAll()
+                        .requestMatchers("/api/v*/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
