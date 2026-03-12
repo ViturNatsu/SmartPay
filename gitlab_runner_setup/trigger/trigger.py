d03fc643d37f3118f03abe0026fce594eed92692
keshavanand <keshavanandsingh89@gmail.com>
2026-03-12 14:24:01 -0400

    added rules for pipeline and webhook. added

--- gitlab_runner_setup/trigger/trigger.py
+++ gitlab_runner_setup/trigger/trigger.py
@@ -1,16 +1,29 @@
 from flask import Flask, request
 import subprocess
-import os   # <-- missing import
+import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def trigger():
     if request.method == "POST":
+        data = request.json
+
+        # only merge request events
+        if data.get("object_kind") != "merge_request":
+            return "Ignored", 200
+
+        # only when MR targets develop
+        target = data["object_attributes"]["target_branch"]
+
+        if target != "develop":
+            return "Ignored - not develop", 200
+
         subprocess.run([
             "gcloud","run","jobs","execute","gitlab-runner","--region","us-central1"
         ])
-        return "Job started", 200
+
+        return "Runner job started", 200
 
     return "Service alive", 200
 
