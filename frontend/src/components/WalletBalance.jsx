34b172e8cd666345e153980677d443ebaf506f4f
justin.latella <justin.latella@fdmgroup.com>
2026-04-24 13:33:06 -0400

    Created dashboard section to display balance initiliazed to $0.00

---
+++ frontend/src/components/WalletBalance.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import { Card, Stack, Typography, ButtonBase, Avatar } from "@mui/material";
+import { useAuth } from "@/context/AuthContext";
+
+function WalletBalance () {
+    const [balance, setBalance] = useState(0.0);
+
+    return (
+        <Card
+            elevation={0} 
+            sx={{ width: "100%", 
+                bgcolor: "#fff", 
+                borderRadius: "16px", 
+                border: "1px solid #E5E7EB", 
+                p: 3 }}
+        >
+            <Stack
+                direction="row"
+                justifyContent="space-between"
+                alignItems="center"
+                mb="20px"
+                sx={{ borderBottom: "1px solid #000000", pb: 2 }}
+            >
+                <Typography variant="h6">
+                    Wallet Balance
+                </Typography>
+                <Typography variant="h6">
+                    ${balance.toFixed(2)}
+                </Typography>
+            </Stack>
+
+        </Card>
+    );
+}
+
+export default WalletBalance;
