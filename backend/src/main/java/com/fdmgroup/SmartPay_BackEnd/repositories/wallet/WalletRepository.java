01c91e867f767ca0e1971141049402d1d87e197f
justin.latella <justin.latella@fdmgroup.com>
2026-04-24 14:01:12 -0400

    Created 'WalletRepository' repository

---
+++ backend/src/main/java/com/fdmgroup/SmartPay_BackEnd/repositories/wallet/WalletRepository.java
@@ -0,0 +1,11 @@
+package com.fdmgroup.SmartPay_BackEnd.repositories.wallet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fdmgroup.SmartPay_BackEnd.domain.entities.wallet.Wallet;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, Long>{
+
+}
