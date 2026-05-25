ba960992bbe5b4bf927d985a3d805c5dfae84e12
NatetheL8 <Natheshan@hotmail.com>
2026-05-25 15:49:53 -0400

    [US 09-01-24] created Payee class

---
+++ backend/src/main/java/com/fdmgroup/SmartPay_BackEnd/domain/entities/payee/Payee.java
@@ -0,0 +1,49 @@
+package com.fdmgroup.SmartPay_BackEnd.domain.entities.payee;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fdmgroup.SmartPay_BackEnd.domain.entities.user.User;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "payees")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Payee {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+@Column(name = "payee_id")
+private long payeeId;
+
+@ManyToOne
+@JoinColumn(name= "owner_user_id", nullable = false)
+private User owner;
+
+@ManyToOne
+@JoinColumn(name = "recipient_user_id", nullable = false)
+private User recipient;
+
+
+
+
+
+
+
+}
