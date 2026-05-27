85caf3497a688801d2e73aafcc81879683c10737
justin.latella <justin.latella@fdmgroup.com>
2026-05-27 15:45:33 -0400

    Updated tests needing to delete tables due to new foreign keys

--- backend/src/test/java/com/fdmgroup/SmartPay_BackEnd/integrationTests/AuthSessionIntegrationTest.java
+++ backend/src/test/java/com/fdmgroup/SmartPay_BackEnd/integrationTests/AuthSessionIntegrationTest.java
@@ -61,6 +61,7 @@ public class AuthSessionIntegrationTest {
 			entityManager.createNativeQuery("DELETE FROM otp").executeUpdate();
             entityManager.createNativeQuery("DELETE FROM chequing_accounts").executeUpdate();
 			entityManager.createNativeQuery("DELETE FROM user_account_table").executeUpdate();
+			entityManager.createNativeQuery("DELETE FROM payment_methods").executeUpdate();
             entityManager.createNativeQuery("DELETE FROM accounts").executeUpdate();
 			entityManager.createNativeQuery("DELETE FROM users").executeUpdate();
 			return null;
