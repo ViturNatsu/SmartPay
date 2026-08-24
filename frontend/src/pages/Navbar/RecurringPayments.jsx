64448c4b5db59a780adecfe3d99244fce61e391f
ericr <Eric.Rohit@fdmgroup.com>
2026-08-24 14:15:12 -0400

    [US-EXP-FE-15] Bugfix for Filter icon

--- frontend/src/pages/Navbar/RecurringPayments.jsx
+++ frontend/src/pages/Navbar/RecurringPayments.jsx
@@ -671,14 +671,6 @@ export default function RecurringPayments() {
 
   }, [billFilters, billsList, billsSearchQuery])
 
-  function FunnelIcon(props) {
-    return (
-      <svg fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
-      </svg>
-    );
-  }
-
   return (
 
     <BasicPageLayout
@@ -748,7 +740,9 @@ export default function RecurringPayments() {
                       }}
                     >
 
-                      <FunnelIcon className="w-4 h-4" />
+                      <svg width={22} height={22} fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+                      </svg>
                       Filter
 
                       {billFilterCount > 0 &&
